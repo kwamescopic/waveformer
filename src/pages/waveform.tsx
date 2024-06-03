13428d44b1b22f56b6635fc5b4b9bd4b3ca34349
Kwame Opare Asiedu <kwame.a@scopicsoftware.com>
2024-06-03 01:30:00 +0000

    Added waveform display. Finished with playback controls (start/pause/restart)

--- src/pages/waveform.tsx
+++ src/pages/waveform.tsx
@@ -1,19 +1,127 @@
 import { Text } from "@/components/text";
 import { useCacheContext } from "@/contexts/cache-context";
-import { useEffect } from "react";
+import { ButtonHTMLAttributes, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
+import WaveSurfer from "wavesurfer.js";
+import { CgSpinnerTwo } from "react-icons/cg";
+import { mergeClasses } from "@/utils";
+import { FaPause, FaPlay } from "react-icons/fa6";
+import { FaUndoAlt } from "react-icons/fa";
 
 export default function WaveformPage() {
+  const waveformRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const { audioFile } = useCacheContext();
+  const [ready, setReady] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [surfer, setSurfer] = useState<WaveSurfer | null>(null);
+  const [playTime, setPlayTime] = useState(0);
+
+  const handleRestart = () => {
+    if (!surfer) return;
+    surfer.stop();
+    // surfer.seekTo(0);
+    setPlaying(false);
+    setPlayTime(0);
+  };
 
   useEffect(() => {
-    if (!audioFile) router.replace("/");
-  }, [audioFile, router]);
+    let audioUrl: string | null = null;
+
+    if (audioFile && waveformRef.current /* && !surfer*/) {
+      audioUrl = URL.createObjectURL(audioFile);
+
+      const wavesurfer = WaveSurfer.create({
+        container: waveformRef.current,
+        url: audioUrl,
+        barGap: 1,
+        barWidth: 2,
+        barRadius: 20,
+        barHeight: 0.5,
+        waveColor: "#4F4A85",
+        progressColor: "#383351",
+      });
+
+      wavesurfer.on("ready", () => setReady(true));
+      wavesurfer.on("timeupdate", (time) => setPlayTime(time));
+      setSurfer(wavesurfer);
+    } else if (!audioFile) {
+      router.replace("/");
+    }
+
+    return () => {
+      if (audioUrl) {
+        URL.revokeObjectURL(audioUrl);
+        surfer?.destroy();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (surfer) {
+      if (playing && !surfer.isPlaying()) {
+        void surfer.play();
+      } else if (!playing && surfer.isPlaying()) {
+        void surfer.pause();
+      }
+    }
+  }, [playing, surfer]);
 
   return (
-    <main className="w-full h-screen grid place-items-center">
-      <Text>Waveform Page</Text>
+    <main className="w-full h-screen grid place-items-center p-8 bg-gray-50">
+      <main className="w-full max-w-[768px] mx-auto flex flex-col items-center justify-center bg-white rounded-xl shadow-2xl border-[1px] border-gray-200 gap-4 p-8">
+        <Text>Waveform Page</Text>
+
+        {!ready && (
+          <div className="flex items-center gap-4">
+            <CgSpinnerTwo className="animate-spin" />
+            <Text>Analyzing...</Text>
+          </div>
+        )}
+
+        <div ref={waveformRef} className="w-full" />
+
+        <div className="flex items-center gap-4">
+          {ready && !playing && (
+            <ControlButton hint="Play" onClick={() => setPlaying(true)}>
+              <FaPlay size={24} color="#fff" />
+            </ControlButton>
+          )}
+
+          {ready && playing && (
+            <ControlButton hint="Pause" onClick={() => setPlaying(false)}>
+              <FaPause size={24} color="#fff" />
+            </ControlButton>
+          )}
+
+          {ready && playTime >= 0 && (
+            <ControlButton hint="Restart" onClick={handleRestart} disabled={playTime === 0}>
+              <FaUndoAlt size={24} color="#fff" />
+            </ControlButton>
+          )}
+        </div>
+      </main>
     </main>
+  );
+}
+
+interface ControlButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  hint: string;
+}
+
+function ControlButton({ className, children, hint, ...rest }: ControlButtonProps) {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <button
+        className={mergeClasses(
+          "grid place-items-center w-12 h-12 bg-sky-400 rounded-full shadow-lg disabled:bg-gray-400",
+          className,
+        )}
+        {...rest}>
+        {children}
+      </button>
+
+      <Text className="text-xs text-gray-400 font-medium">{hint}</Text>
+    </div>
   );
 }
